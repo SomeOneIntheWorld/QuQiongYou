83bc682e2e67996bd840d7ed647ecebc1a73fa0b
cpb <1198960858@qq.com>
2016-10-03 12:55:53 +0800

    add packages

--- app/src/main/java/dream/quqiongyou/main/LoginActivity.java
+++ app/src/main/java/dream/quqiongyou/main/LoginActivity.java
@@ -1,7 +1,9 @@
-package dream.quqiongyou;
+package dream.quqiongyou.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dream.quqiongyou.R;
 
 public class LoginActivity extends AppCompatActivity {
 
