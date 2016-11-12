c4a5c71ea28b32d8992fea36b6cd79afa25d12b1
cpb <1198960858@qq.com>
2016-11-13 00:25:46 +0800

    beauty the layout of fragmen_mine

--- app/src/main/java/dream/quqiongyou/bean/TopInfo.java
+++ app/src/main/java/dream/quqiongyou/bean/TopInfo.java
@@ -17,11 +17,11 @@ public class TopInfo {
         this.title = title;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
